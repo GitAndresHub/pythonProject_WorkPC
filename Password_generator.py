fc6ff31fd4c299023165dac73ce2a3a27c935840
AndresLõhmus <andres.lohmus@gmail.com>
2023-02-14 16:08:14 +0200

    Initial files for practicing, additions from 14.02

--- Password_generator.py
+++ Password_generator.py
@@ -3,20 +3,17 @@ import string
 
 
 def generate_password():
-    instructions = input("How many characters do you want in your password?")
+    instructions = input("How many characters do you want in your password? ")
     char_num = int(instructions)
     password = []
     i = 0
-    while i <= char_num:
-        char = random.choice(string.ascii_letters)
-        num = random.randint(0, 9)
-        password = str(char + str(num))
-        print(password)
-        password = list(password)
-        print(password)
-        password = password.append(password)
-        i += 1
-        print(password)
+    for i in range(char_num):
+        while i <= char_num:
+            char = random.choice(string.ascii_letters)
+            num = random.randint(0, 9)
+            password = str(char + str(num))
+            i += 1
+        print(password, end="")
 
 
 generate_password()
