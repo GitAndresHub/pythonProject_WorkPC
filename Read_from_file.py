ce08a492c853bcc98f57b82048f771af6b9932c0
Andres Lõhmus <70094845+GitAndresHub@users.noreply.github.com>
2023-02-26 23:12:53 +0200

    Updates 26.02, new exercises

--- Read_from_file.py
+++ Read_from_file.py
@@ -3,3 +3,16 @@ with open("C:/Users/Arvuti3/Documents/test_file.txt", "r") as names:
     list_names = list(all_names.split("\n"))
     my_dict = {i: list_names.count(i) for i in list_names}
     print(my_dict)
+
+counter_dict = {}
+with open("C:/Users/Arvuti3/Documents/test_file.txt", "r") as f:
+	line = f.readline()
+	while line:
+		line = line.strip()
+		if line in counter_dict:
+			counter_dict[line] += 1
+		else:
+			counter_dict[line] = 1
+		line = f.readline()
+
+print(counter_dict)
