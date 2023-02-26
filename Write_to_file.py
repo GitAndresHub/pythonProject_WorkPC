5a0a3d3cd0c552401d361149cd54f73f45f5dd11
Andres Lõhmus <70094845+GitAndresHub@users.noreply.github.com>
2023-02-26 19:03:33 +0200

    Updates 15.02, new exercises

--- Write_to_file.py
+++ Write_to_file.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 
 url = "https://www.nytimes.com/"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 headlines = soup.find_all(class_="css-xdandi")
 
-with open("NYT.txt", "w") as open_file:
+file_name = input("Please enter file name: ")
+
+with open(f"C:/Users/Arvuti3/Documents/{file_name}.txt", "w") as open_file:
     for headline in headlines:
         open_file.write(headline.text.strip())
